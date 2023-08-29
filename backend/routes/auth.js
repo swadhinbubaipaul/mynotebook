f80ab73d3472e1f57f76a0ea08e6ba45be739c17
swadhinbubaipaul <swadhinpaul159@gmail.com>
2023-08-29 13:17:35 +0530

    login endpoint created

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,7 +7,7 @@ var jwt = require("jsonwebtoken");
 
 const JWT_SECRET = "Swadhinisagoodboy$";
 
-// Create a user using POST "/api/auth". Doesn't require Auth
+// Create a user using POST "/api/auth/createuser". No ligin required
 router.post(
 	"/createuser",
 	[
@@ -44,43 +44,91 @@ router.post(
 				async function (err, hash) {
 					// Store hash in your password DB.
 					// Create a new user
-					if (err) {
-						console.error("err");
-						res.status(500).json({
-							success: false,
-							error: "Some error occured",
-						});
-					} else {
-						user = new User({
-							name: req.body.name,
-							password: hash,
-							email: req.body.email,
-						});
+					user = new User({
+						name: req.body.name,
+						password: hash,
+						email: req.body.email,
+					});
 
-						// Save it in db
-						await user.save();
+					// Save it in db
+					await user.save();
 
-						const data = {
-							user: {
-								id: user.id,
-							},
-						};
+					const data = {
+						user: {
+							id: user.id,
+						},
+					};
 
-						const authToken = jwt.sign(data, JWT_SECRET);
+					const authToken = jwt.sign(data, JWT_SECRET);
 
-						res.json({
-							success: true,
-							message: "User created",
-							authToken: authToken,
-						});
-					}
+					res.json({
+						success: true,
+						message: "User created",
+						authToken: authToken,
+					});
 				}
 			);
 		} catch (error) {
 			console.error(error.message);
 			res.status(500).json({
 				success: false,
-				error: "Some error occured",
+				error: "Internal server error",
+			});
+		}
+	}
+);
+
+// Create a user using POST "/api/auth/login".
+router.post(
+	"/login",
+	[
+		body("email", "Enter a valid email").isEmail(),
+		body("password", "Password cannot be blank").exists(),
+	],
+	async (req, res) => {
+		// If there are errors, return Bad request and errors
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res
+				.status(400)
+				.send({ success: false, errors: errors.array() });
+		}
+
+		const { email, password } = req.body;
+		try {
+			let user = await User.findOne({ email });
+			if (!user) {
+				return res.status(400).json({
+					success: false,
+					error: "Please try to login with correct credentials",
+				});
+			}
+
+			bcrypt.compare(password, user.password, function (err, result) {
+				if (!result) {
+					return res.status(400).json({
+						success: false,
+						error: "Please try to login with correct credentials",
+					});
+				}
+				const data = {
+					user: {
+						id: user.id,
+					},
+				};
+				const authToken = jwt.sign(data, JWT_SECRET);
+
+				res.json({
+					success: true,
+					message: "Login successful",
+					authToken: authToken,
+				});
+			});
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).json({
+				success: false,
+				error: "Internal server error",
 			});
 		}
 	}
