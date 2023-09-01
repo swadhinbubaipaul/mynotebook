b2c038a78864ac0f00e287dcff1ef749330b8b43
swadhinbubaipaul <swadhinpaul159@gmail.com>
2023-09-01 17:56:59 +0530

    added fetchallnotes and addnote endpoints

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -2,22 +2,26 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const notesSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  tag: {
-    type: String,
-    default: "General",
-  },
-  date: {
-    type: Date,
-    default: Date.now,
-  },
+	user: {
+		type: Schema.Types.ObjectId,
+		ref: "user",
+	},
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	tag: {
+		type: String,
+		default: "General",
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
 });
 
 module.exports = mongoose.model("notes", notesSchema);
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,62 @@
 const express = require("express");
 const router = express.Router();
+const Notes = require("../models/Notes");
+const { body, validationResult } = require("express-validator");
+const fetchUser = require("../middleware/fetchUser");
 
-router.get("/", (req, res) => {
-	res.json([]);
+// ROUTE 1: Get all the notes using GET "/api/notes/fetchallnotes". Login required
+router.get("/fetchallnotes", fetchUser, async (req, res) => {
+	const notes = await Notes.find({ user: req.user.id });
+	res.json(notes);
 });
+
+// ROUTE 2: Add a new note using GET "/api/notes/addnote". Login required
+router.post(
+	"/addnote",
+	fetchUser,
+	[
+		body("title", "Enter a valid title").isLength({ min: 3 }),
+		body(
+			"description",
+			"Description must be at least 5 characters"
+		).isLength({ min: 5 }),
+	],
+	async (req, res) => {
+		// If there are errors, return Bad request and errors
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res
+				.status(400)
+				.send({ success: false, errors: errors.array() });
+		}
+
+		try {
+			const { title, description, tag } = req.body;
+			// Create a new user
+			const note = new Notes({
+				user: req.user.id,
+				title: title,
+				description: description,
+				tag: tag,
+			});
+
+			// Save it in db
+			await note.save();
+
+			// Send response
+			res.json({
+				success: true,
+				message: "Note saved successfully",
+				note: note,
+			});
+		} catch (error) {
+			console.error(error.message);
+			res.status(500).json({
+				success: false,
+				error: "Internal server error",
+			});
+		}
+	}
+);
 
 module.exports = router;
